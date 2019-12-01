4ab3cd0f5d35be0539312eee73045a77e3938b8e
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2019-12-01 20:31:50 +0530

    Add link to resume

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ console.log(`
 Name: Chaitanya Deorukhkar
 Twitter: @_anothercoder
 Github: https://github.com/deorukhkarchaitanya
+Resume: http://bit.ly/chaitanyad
 
 Writes React.js & React Native. Rants about tech. 
 
