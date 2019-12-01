6ecdb5bbdd9faee5227e506c0754da9f5ec7c277
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2019-12-01 20:24:31 +0530

    Create a basic npx profile package

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+console.log(`
+---------------------------------------------------
+
+Name: Chaitanya Deorukhkar
+Twitter: @_anothercoder
+Github: https://github.com/deorukhkarchaitanya
+
+Writes React.js & React Native. Rants about tech. 
+
+---------------------------------------------------
+`);
